972622c8697f1a266eaefaa6bf68a5e2d0c24f84
Leigh Johnson <leigh@bitsy.ai>
2022-11-17 16:42:25 -0800

    add test for mgmt cmd

---
+++ django_ghost/management/commands/django_ghost_sync.py
@@ -0,0 +1,15 @@
+from click import BaseCommand
+from django.core.management.base import BaseCommand
+
+from django_ghost.services import update_or_create_ghost_member
+from django_ghost.settings import django_ghost_settings
+
+GhostMemberModel = django_ghost_settings.get_member_model()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        members_to_sync = GhostMemberModel.objects.all()
+
+        for member in members_to_sync:
+            update_or_create_ghost_member(member)
